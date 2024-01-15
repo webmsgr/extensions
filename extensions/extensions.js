4b64480cd73bf1fe5af57b489305df26300d7690
webmsgr <zapcontact000@gmail.com>
2024-01-14 20:24:06 -0800

    Use a virtual list instead of just putting all of the extensions in the DOM.

--- extensions/extensions.js
+++ extensions/extensions.js
@@ -32,6 +32,69 @@
     }
 
     document.addEventListener("alpine:init", () => {
+        Alpine.directive("virtual", (template_el, { value, modifiers, expression }, { Alpine, effect, cleanup, evaluateLater }) => {
+            console.log("setting up virtual list with template", template_el)
+            let el = document.createElement("div");
+            //el.setAttribute("x-data", "{}");
+            template_el.parentElement.appendChild(el);
+            //el.style.flexGrow = 1;
+            //el.style.height = + "px";
+            console.log("setting up virtual list under", el)
+            let [raw_key, bind_name] = expression.split(":");
+            let getKeyData = evaluateLater(raw_key);
+
+            function createElement(index) {
+                const element = document.createElement('div');
+                element.style.width = '100%';
+                element.appendChild(template_el.content.cloneNode(true));
+                getKeyData(key => {
+                    element.setAttribute("x-data", JSON.stringify({[bind_name]: key[index]}));
+                })
+                return element;
+            }
+            let list = null;
+            function updateList() {
+                getKeyData(key => {
+                    let options = {
+                        width: '100%',
+                        height: window.innerHeight - el.offsetTop ,
+                        itemHeight: 55, // aprox
+                        generate: createElement,
+                        total: key.length
+                    }
+                    if (list === null) {
+                        list = new HyperList(el, options);
+                        // this causes alpine to actually init the elements on first render
+                        // if you don't do this, you need to scroll for anything to show up (bad ux)
+                        setTimeout(() => Alpine.initTree(el), 0);
+                        if (document.location.hash) {
+                            let scroll_to = document.location.hash.substring(1);
+                            // find that in the list
+                            let index = key.findIndex((i) => i.pkg.replace('eu.kanade.tachiyomi.extension.', '') == scroll_to);
+                            if (index > -1) {
+                                // from https://github.com/tbranyen/hyperlist/issues/48#issuecomment-1495185044
+                                console.log("scrolling to", index, list._itemPositions[index])
+                                list._config.overrideScrollPosition = () => list._itemPositions[index] 
+                                list._renderChunk()
+                                list._config.overrideScrollPosition = null
+                                el.scrollTo(0, list._itemPositions[index])
+                            }
+                        }
+                    } else {
+                        list.refresh(el, options);
+                    }
+                })
+            }
+            effect(() => {
+                updateList();
+            })
+            window.addEventListener('resize', updateList);
+            cleanup(() => {
+                window.removeEventListener('resize', updateList);
+
+            })
+            
+        })
         Alpine.store("repoUrl", "https://raw.githubusercontent.com/keiyoushi/extensions/repo");
 
         Alpine.data("extensionList", () => ({
@@ -88,6 +151,7 @@
                 this.$nextTick(() => {
                     window.location.hash && window.location.replace(window.location.hash);
                 });
+
             },
 
             updateFilteredList() {
